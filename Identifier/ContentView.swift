45d9965a2c96debb97a049e11067a14fbdd6f168
yasirkhan1301 <mr_yasir_khan52@yahoo.com>
2019-12-18 20:19:53 +0500

    First Commit

---
+++ Identifier/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Identifier
+//
+//  Created by Yasir khan on 18/12/2019.
+//  Copyright © 2019 Yasir khan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
